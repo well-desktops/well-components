48b97a681433be11b04a07b544aeaf74772b09d9
Taras Chornyi <taras.chornyi@compart.com>
2020-11-24 09:47:58 +0100

    Added prismjs library for html code tokenization

--- src/utility/index.ts
+++ src/utility/index.ts
@@ -1,6 +1,11 @@
-import PrismJS from 'prismjs';
+// import PrismJS from 'prismjs';
+import PrismJS from './prism.js';
 
 export const highlight = (source: string) => {
   const highlighted = PrismJS.highlight(source, PrismJS.languages.html, 'html');
   return `<pre><code>${highlighted}</code></pre>`;
 }
+
+
+
+
